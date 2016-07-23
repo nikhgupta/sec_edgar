f7815218ad35d1b0a07a8b1fa6cbdece852a5a74
Nikhil Gupta <mestoic@gmail.com>
2016-07-23 20:01:49 +0530

    fix

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -75,7 +75,7 @@ ActiveAdmin.register_page "Dashboard" do
 
     div class: "empty-html-reports", style: "margin-top: 20px" do
       h3 "Empty HTML Reports (they will be queued again in the last)"
-      ul(style: "columns: 3; -webkit-columns: 3; -moz-columns: 3") do
+      ul(style: "columns: 3; -webkit-columns: 3; -moz-columns: 3; font-family: monospace; font-size: 11px") do
         raw Report.where(empty_html: true).map(&:name).sort_by(&:itself).map{|rn| "<li>#{rn}</li>"}.join
       end
     end
